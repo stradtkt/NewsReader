c6a8feb9521cf79b447a0ab1457d083addf5be84
Kevin <stradtkt22@gmail.com>
2019-08-27 12:32:30 -0400

    Added connection to get the data from the website

--- app/src/main/java/com/stradtman/newsreader/MainActivity.java
+++ app/src/main/java/com/stradtman/newsreader/MainActivity.java
@@ -2,13 +2,66 @@ package com.stradtman.newsreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    ArrayList<String> titles = new ArrayList<String>();
+    ArrayAdapter arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.listView);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titles);
+        listView.setAdapter(arrayAdapter);
+
+        DownloadTask task = new DownloadTask();
+        try {
+            task.execute("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(strings[0]);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlConnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+                int data = reader.read();
+                while(data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                Log.i(TAG, "doInBackground: URLContent: " + result);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
     }
 }
